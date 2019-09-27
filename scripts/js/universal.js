acba09684b1b11fbd6920d841641550fd80d5814
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-27 14:01:00 +0100

    Form validation

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -8,10 +8,32 @@ const app = new Vue({
     navPull: false,
     outlineHide: true,
   },
-
-  methods: {
-    formValidate() {
-      alert('Hello');
-    },
-  },
 });
+
+const formInput = document.querySelectorAll('input');
+const formSelect = document.querySelectorAll('select');
+const emailInput = document.querySelector('.email-input');
+
+console.log(emailInput.value);
+
+function formValidate(){
+  this.classList.add('form-input-validation');
+}
+
+function emailCheck() {
+  if (this.value.length === 0) {
+    emailInput.classList.add('not-valid');
+  } else {
+    emailInput.classList.remove('not-valid');
+  }
+}
+
+for (let i = 0; i < formInput.length; i++) {
+  formInput[i].addEventListener('blur', formValidate);
+}
+
+for (let i = 0; i < formSelect.length; i++) {
+  formSelect[i].addEventListener('blur', formValidate);
+}
+
+emailInput.addEventListener('blur', emailCheck);
