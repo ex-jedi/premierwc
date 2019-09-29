7369da96e4a6838cc71ceb232e4932653f9c5cb1
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-29 20:38:47 +0100

    Removing section wrapper for map &n adding border

--- contact.php
+++ contact.php
@@ -1,9 +1,7 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content contact-main-content">
-	<section>
-	  <?php perch_content("Google Map"); ?>
-	</section>
+	<?php perch_content("Google Map"); ?>
 	<?php perch_content("Contact Form Heading"); ?>
 	<?php perch_content("Contact Form"); ?>
 </main>
