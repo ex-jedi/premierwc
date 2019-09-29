b39a08fd3b3053f5c0fe5bb74d5a2d05d4f4965f
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-29 20:48:21 +0100

    Changing map template to perch layout

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php include('perch/runtime.php'); ?>
-<?php perch_layout('main-header'); ?>
+<?php perch_layout("main-header"); ?>
 <main class="main-content contact-main-content">
-	<?php perch_content("Google Map"); ?>
+	<?php perch_layout("google-map"); ?>
 	<?php perch_content("Contact Form Heading"); ?>
 	<?php perch_content("Contact Form"); ?>
 </main>
